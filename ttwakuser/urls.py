e8fd652e5c96e3891566bb10d2cbb0f45f2dc435
Seolmango <seolchaehwan@naver.com>
2023-06-28 18:24:52 +0900

    사소한 에러 해결 및 슈퍼 유저 추가

--- ttwakuser/urls.py
+++ ttwakuser/urls.py
@@ -4,5 +4,5 @@ from django.contrib.auth import views as auth_views
 app_name = 'ttwakuser'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='ttwakuser/login.html')
+    path('login/', auth_views.LoginView.as_view(template_name='ttwakuser/login.html'))
 ]
